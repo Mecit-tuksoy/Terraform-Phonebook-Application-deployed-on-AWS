d2bac3588c9e53825f35f66463816b4f61f89a78
Mecit-tuksoy <mecit.tuksoy@gmail.com>
2023-12-22 11:52:15 +0300

    new

--- main.tf
+++ main.tf
@@ -1,75 +1,186 @@
-provider "aws" {
-  region  = "us-east-1"
-}
-
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.30.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-resource "aws_db_instance" "example_rds" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "8.0.28"
-  instance_class       = "db.t2.micro"
-  username             = "admin"
-  password             = "123456789"
-  skip_final_snapshot  = true
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1"
 }
-resource "aws_security_group" "rds_sg" {
-  name        = "rds_sg"
-  description = "RDS Security Group"
 
+resource "aws_db_instance" "db-server" {
+  instance_class              = "db.t2.micro"
+  allocated_storage           = 20
+  vpc_security_group_ids      = [aws_security_group.db-sg.id]
+  allow_major_version_upgrade = false
+  auto_minor_version_upgrade  = true
+  backup_retention_period     = 0
+  identifier                  = "phonebook-app-db"
+  db_name                     = "phonebook"
+  engine                      = "mysql"
+  engine_version              = "8.0.28"
+  username                    = "admin"
+  password                    = "Oliver_1"
+  monitoring_interval         = 0
+  multi_az                    = false
+  port                        = 3306
+  publicly_accessible         = false
+  skip_final_snapshot         = true
+}
+
+resource "aws_security_group" "db-sg" {
+  name   = "RDSSecurityGroup"
+  vpc_id = data.aws_vpc.selected.id
+  tags = {
+    "Name" = "TF_RDSSecurityGroup"
+  }
   ingress {
-    from_port = 3306
-    to_port   = 3306
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Uygun güvenlik politikasına göre ayarlayın
+    cidr_blocks = ["0.0.0.0/0"]   # Attention
+    from_port       = 3306
+    protocol        = "tcp"
+    to_port         = 3306
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = -1
+    to_port     = 0
   }
 }
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2_sg"
-  description = "EC2 Security Group"
+
+resource "aws_security_group" "server-sg" {
+  name   = "WebServerSecurityGroup"
+  vpc_id = data.aws_vpc.selected.id
+  tags = {
+    Name = "TF_WebServerSecurityGroup"
+  }
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Uygun güvenlik politikasına göre ayarlayın
+    from_port       = 80
+    protocol        = "tcp"
+    to_port         = 80
+    security_groups = [aws_security_group.alb-sg.id]   # Attention
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-output "rds_endpoint" {
-  value = aws_db_instance.example_rds.endpoint
+resource "aws_security_group" "alb-sg" {
+  name   = "ALBSecurityGroup"
+  vpc_id = data.aws_vpc.selected.id
+  tags = {
+    Name = "TF_ALBSecurityGroup"
+  }
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_instance" "example_ec2" {
-  depends_on = [aws_db_instance.example_rds]
-  ami           = "ami-079db87dc4c10ac91"
-  instance_type = "t2.micro"
-  key_name = "firstkey"
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  user_data = <<-EOF
-              #! /bin/bash -x
-              yum update -y
-              yum install python3 -y
-              yum install python-pip -y
-              pip3 install Flask==2.3.3
-              pip3 install Flask-MySql
-              pip3 install boto3
-              yum install git -y
-              yum install -y mysql
-              mysql -h aws_db_instance.example_rds.endpoint -u admin -p 123456789 -e "SHOW DATABASES;"
-              TOKEN="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" 
-              cd /home/ec2-user
-              git clone https://$TOKEN@github.com/Emrkts/Phonebook-Web-App.git
-              python3 /home/ec2-user/Phonebook-Web-App/phonebook-app.py
-      EOF
+
+
+
+resource "aws_launch_template" "asg-lt" {
+  name                   = "phonebook-lt"
+  image_id               = data.aws_ami.al2023.id
+  instance_type          = "t2.micro"
+  key_name               = var.key-name
+  vpc_security_group_ids = [aws_security_group.server-sg.id]
+  user_data              = base64encode(templatefile("user-data.sh", { db-endpoint = aws_db_instance.db-server.address, user-data-git-token = var.git-token, user-data-git-name = var.git-name }))
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "Web Server of Phonebook App"
+    }
+  }
+}
+
+resource "aws_alb" "app-lb" {
+  name               = "phonebook-lb-tf"
+  ip_address_type    = "ipv4"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb-sg.id]
+  subnets            = data.aws_subnets.pb-subnets.ids
 
 }
+
+resource "aws_alb_listener" "app-listener" {
+  load_balancer_arn = aws_alb.app-lb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.app-lb-tg.arn
+  }
+}
+
+resource "aws_alb_target_group" "app-lb-tg" {
+  name        = "phonebook-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.selected.id
+  target_type = "instance"
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+}
+
+resource "aws_autoscaling_group" "app-asg" {
+  max_size                  = 3
+  min_size                  = 1
+  desired_capacity          = 1
+  name                      = "phonebook-asg"
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  target_group_arns         = [aws_alb_target_group.app-lb-tg.arn]
+  vpc_zone_identifier       = aws_alb.app-lb.subnets
+  launch_template {
+    id      = aws_launch_template.asg-lt.id
+    version = aws_launch_template.asg-lt.latest_version
+  }
+}
+
+resource "aws_route53_record" "phonebook" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "phonebook.${var.hosted-zone}"
+  type    = "A"
+
+  alias {
+    name                   = aws_alb.app-lb.dns_name
+    zone_id                = aws_alb.app-lb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+
+
+
